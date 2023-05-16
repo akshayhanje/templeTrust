6ebb479b35cbbce123fedb96a015f47b57842c65
Akshay Hanje <52993534+akshayhanje@users.noreply.github.com>
2023-05-16 09:13:46 +0530

    Add files via upload

---
+++ akshay/Contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel ="stylesheet" href="style.css">
+
+    </head>
+    <body id="homepage">
+        <div id="container">
+            <header><img src="./img/temple2.jpg" alt="Header Image" class="img-Header" id="Manstambha">
+            </header>
+            <ul class="navmenu">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Contact.php">Contact</a>
+                </li>
+               <li><a href="#">Dashboard</a></li>
+            </ul>
+        </div>
+        <div class="contact">
+            <div class="info">
+                <h3>Contact Us</h3>
+              <p>Address: 123 Main Street, Anytown USA</p>
+              <p>Phone: (123) 456-7890</p>
+              <p>Email: info@example.com</p>
+            </div>
+            <img src="./img/jain1.jpg" alt="Image Description" width="400" height="250">
+          </div>
+                   
+    </body>
+</html>
+
